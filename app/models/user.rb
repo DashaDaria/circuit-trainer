036c3bd3637149f789822da68aeae045ba288b88
Dasha Nikitina <dashadariallc@gmail.com>
2017-11-20 05:33:25 -0500

    testing index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
-  
+
   has_many :circuits
   has_many :exercises, through: :circuits
+  has_secure_password
 end
