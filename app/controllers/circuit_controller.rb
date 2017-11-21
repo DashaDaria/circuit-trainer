a4b12ab16cc22a3915e6c7f3e4d3b7bf47cfe47a
Dasha Nikitina <dashadariallc@gmail.com>
2017-11-21 03:50:57 -0500

    adding circuit routes

---
+++ app/controllers/circuit_controller.rb
@@ -0,0 +1,21 @@
+class UserController < ApplicationController
+
+  get '/circuits' do
+    if logged_in?
+      @user = current_user
+    erb :'circuits/circuits'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/circuits/new' do
+    if logged_in?
+      erb :'circuits/new'
+    else
+      redirect '/login'
+    end
+  end
+
+
+end
